d65b0120feb2eca72d15ec1cedf53b96dfb6e632
Kshitiz Anand <k.anand26@gmail.com>
2015-09-26 18:46:16 +0530

    Added next button etc

--- app/src/main/java/com/anandkshitiz/geoquiz/QuizActivity.java
+++ app/src/main/java/com/anandkshitiz/geoquiz/QuizActivity.java
@@ -6,12 +6,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+        new TrueFalse(R.string.question_oceans, true),
+        new TrueFalse(R.string.question_mideast, false),
+        new TrueFalse(R.string.question_africa, false),
+        new TrueFalse(R.string.question_americas, true),
+        new TrueFalse(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/anandkshitiz/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.anandkshitiz.geoquiz;
+
+/**
+ * Created by Kshitiz on 26-09-2015.
+ */
+public class TrueFalse {
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        this.mQuestion = question;
+        this.mTrueQuestion = trueQuestion;
+    }
+}
