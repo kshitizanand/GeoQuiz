0c1d15263334ba6681846165c07de9e378848829
Kshitiz Anand <k.anand26@gmail.com>
2015-10-04 22:39:18 +0530

    Added Intent

--- app/src/main/java/com/anandkshitiz/geoquiz/CheatActivity.java
+++ app/src/main/java/com/anandkshitiz/geoquiz/CheatActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
  * Created by Kshitiz on 03-10-2015.
  */
 public class CheatActivity extends Activity {
+
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.anandkshitiz.geoquiz.answer_is_true";
+    private boolean mAnswerIsTrue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
-
-
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
     }
 }
--- app/src/main/java/com/anandkshitiz/geoquiz/QuizActivity.java
+++ app/src/main/java/com/anandkshitiz/geoquiz/QuizActivity.java
@@ -93,6 +93,8 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //start Activity
                 Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean ans = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, ans);
                 startActivity(i);
             }
         });
